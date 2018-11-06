a07d35a2d4aefb4d743b67a50c4ec82cef46af21
yanlin li <yanlinli@yanlins-MacBook-Pro.local>
2018-11-06 10:11:06 -0800

    added one more function for practice

--- recur.cc
+++ recur.cc
@@ -4,13 +4,13 @@
 int fib(int n);		
 
 //TODO #2.1: add a function prototype for fact
+int fac(int n);
 
 int main()
 {
 	//TODO: #1.2 call your fib function, print the result
-	
-	//TODO: #2.2 call your fact function, print the result
-
+	fib(5);
+	fac(10);	
 	return 0;
 }
 
@@ -25,6 +25,13 @@ int fib(int n){
 	return fib(n-2)+ fib(n-1);
 }
 //TODO #2.4: add your recursive fact function from class 
+int fac(int n){
+	if(n == 0){
+	return 1;	
+	}
+	
+	return n*fact(n-1);
 
+}
 
 
